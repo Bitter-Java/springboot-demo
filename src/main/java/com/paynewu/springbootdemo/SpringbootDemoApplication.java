190eabaa852b6c4aa5d87e589011b42ac922d7b2
payne-wu <wuzhipeng1289690157@gmail.com>
2023-08-21 14:30:28 +0800

    update

--- src/main/java/com/paynewu/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/paynewu/springbootdemo/SpringbootDemoApplication.java
@@ -1,5 +1,7 @@
 package com.paynewu.springbootdemo;
 
+import java.util.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author <a href="https://paynewu.com">PayneWu</a>
- * @github <a href="https://github.com/payne-wu">payne-wu</a>
+ * @github <a href="https://github.com/0x2cf">payne-wu</a>
  * @date 19:19 2023/1/15
  * @project springboot-demo
  * @description
@@ -16,13 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringbootDemoApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SpringbootDemoApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
 
+    @RequestMapping("/")
+    public String index() {
+        return "hello, Springboot";
+    }
 
-  @RequestMapping("/")
-  public String index() {
-    return "hello, Springboot";
-  }
+    @RequestMapping("/hello")
+    public Map<String, String> hello() {
+        return new HashMap<String, String>(2) {{
+            put("hello", "World");
+        }};
+    }
 }
